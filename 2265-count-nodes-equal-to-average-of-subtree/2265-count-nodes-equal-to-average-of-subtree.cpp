4ff641076b6f5d25c5bd762005d66c0adadc600e
Aditya Kumar <01aditya1006@gmail.com>
2026-09-10 23:41:56 +0530

    Time: 3 ms (91.06%), Space: 15.9 MB (29.76%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -11,10 +11,10 @@
  */
 class Solution {
 public:
-    pair<int,int> FindAvg(int& n, TreeNode* root, int& ans) {
+    pair<int,int> FindAvg(TreeNode* root, int& ans) {
         if(root==NULL) return {0,0};
-        auto [l,dl]=FindAvg(n,root->left,ans);
-        auto [r,dr]=FindAvg(n,root->right,ans);
+        auto [l,dl]=FindAvg(root->left,ans);
+        auto [r,dr]=FindAvg(root->right,ans);
 
         int val=root->val;
         if((l+r+val)/(dl+dr+1)==val) {
@@ -23,19 +23,8 @@ public:
         return {l+r+val,dl+dr+1};
     }
     int averageOfSubtree(TreeNode* root) {
-        TreeNode* temp=root;
-        queue<TreeNode*>q;
-        q.push(root);
-        int n=0;
-        while(!q.empty()) {
-            n++;
-            TreeNode* tp=q.front();
-            q.pop();
-            if(tp->left) q.push(tp->left);
-            if(tp->right) q.push(tp->right);
-        }
         int ans=0;
-        FindAvg(n,root,ans);
+        FindAvg(root,ans);
         return ans;
     }
 };
